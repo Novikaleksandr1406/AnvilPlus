4d962dc5bb5b59e73c0d7d8e4463f35719c6809e
Sahsa1_1 <88210386+Novikaleksandr1406@users.noreply.github.com>
2024-07-07 01:04:29 +0300

    Add files via upload

---
+++ src/main/java/com/sahsa1_1/anvil_plus/AnvilDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sahsa1_1.anvil_plus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnvilDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
